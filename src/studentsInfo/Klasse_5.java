7d7a70eedcb3142b3ea147ca72efbd96501ddc53
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-10 23:24:29 +0200

    Update Klasse_5.java

--- src/studentsInfo/Klasse_5.java
+++ src/studentsInfo/Klasse_5.java
@@ -1,2 +1,10 @@
-package studentsInfo;public class Klasse_5 {
+package studentsInfo;
+
+public class Klasse_5 implements IMessage{
+    @Override
+    public void msg() {
+        System.out.println("wir freuen uns darauf, " +
+                "Ihre Verfahren in Ihrer neuen Schule nach einem schönen Urlaub abzuschließen");
+    }
 }
+
