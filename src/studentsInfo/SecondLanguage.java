487500ea34f82e1977bd7a1b064f681d747f742b
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-10 23:23:52 +0200

    Update SecondLanguage.java

--- src/studentsInfo/SecondLanguage.java
+++ src/studentsInfo/SecondLanguage.java
@@ -1,2 +1,25 @@
-package studentsInfo;public class SecondLanguage {
+package studentsInfo;
+
+public class SecondLanguage extends DataBase{
+    private String secondLanguage;
+
+    public SecondLanguage(String name, String division, int studentId, String classId, String secondLanguage) {
+        super(name, division, studentId, classId);
+        this.secondLanguage=secondLanguage;
+    }
+
+
+    public String getSecondLanguage() {
+        return secondLanguage;
+    }
+
+    public void setSecondLanguage(String secondLanguage) {
+        this.secondLanguage = secondLanguage;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: "+getName()+", Division: "+getDivision()+", Student ID: "+getStudentId()+
+                ", Klasse ID: "+getClassId()+"Second Language: "+secondLanguage;
+    }
 }
