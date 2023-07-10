015bde023d977a8e417cfafb6b020df3f0fca4e9
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-10 23:24:45 +0200

    Update Klasse_6.java

--- src/studentsInfo/Klasse_6.java
+++ src/studentsInfo/Klasse_6.java
@@ -1,2 +1,12 @@
-package studentsInfo;public class Klasse_6 {
+package studentsInfo;
+
+public class Klasse_6 implements IMessage{
+
+    @Override
+    public void msg() {
+        System.out.println("Wir freuen uns auf einen schönen Urlaub mit der ersten Erfahrung, " +
+                "das erste Jahr zu absolvieren und dann den Lernweg fortzusetzen.");
+        System.out.println("Übrigens viel Spaß beim neuen Sprachlernen");
+    }
 }
+
